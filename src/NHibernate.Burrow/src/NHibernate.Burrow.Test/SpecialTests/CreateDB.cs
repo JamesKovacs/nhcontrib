5a27b7e134aef360c57e185ba496fe846aca2aeb
kailuowang <kailuowang@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-02-08 03:21:49 +0000

    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@20 d7b3437e-3345-0410-94a8-cbd290e69f67

--- src/NHibernate.Burrow/src/NHibernate.Burrow.Test/SpecialTests/CreateDB.cs
+++ src/NHibernate.Burrow/src/NHibernate.Burrow.Test/SpecialTests/CreateDB.cs
@@ -1,9 +1,10 @@
 using NHibernate.Burrow.Configuration;
+using NHibernate.Burrow.TestUtil;
 using NUnit.Framework;
 
 namespace NHibernate.Burrow.Test.SpecialTests {
     [TestFixture]
-    public class CreateDB {
+    public class CreateDB : TestBase {
         [Test, Explicit]
         public void CreateTestDataBase() {
             Config.CreateDatabase();
