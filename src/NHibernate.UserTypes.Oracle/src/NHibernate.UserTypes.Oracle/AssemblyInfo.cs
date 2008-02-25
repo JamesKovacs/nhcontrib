d7017315c3ff666cfeae08071bfb59351fb2c2bb
karlchu <karlchu@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-02-25 00:22:04 +0000

    NHibernate.UserTypes.Oracle - Moved here from NHibernate core
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@50 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.UserTypes.Oracle/src/NHibernate.UserTypes.Oracle/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+using System.Security;
+
+[assembly: CLSCompliantAttribute(true)]
+[assembly: AssemblyTitleAttribute("NHibernate.UserTypes.Oracle")]
+[assembly: AssemblyDescriptionAttribute("Custom Oracle-specific types for NHibernate 2.0.0.Alpha1")]
+[assembly: AssemblyCompanyAttribute("NHibernate.org")]
+[assembly: AssemblyProductAttribute("NHibernate.UserTypes.Oracle")]
+[assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
+[assembly: AssemblyVersionAttribute("2.0.0.1001")]
+[assembly: AssemblyInformationalVersionAttribute("2.0.0.1001")]
+[assembly: AssemblyFileVersionAttribute("2.0.0.1001")]
+//[assembly: AssemblyKeyFileAttribute("../../src/NHibernate.snk")]
+[assembly: AssemblyDelaySignAttribute(false)]
+[assembly: AllowPartiallyTrustedCallers()]
