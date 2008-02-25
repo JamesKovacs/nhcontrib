940e151edfef4311df92109dcde5a32ed97949b7
karlchu <karlchu@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-02-25 00:32:41 +0000

    NHibernate.UserTypes.SqlTypes - Moved here from NHibernate core
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@51 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.UserTypes.SqlTypes/src/NHibernate.UserTypes.SqlTypes.Tests/SerializabilityFixture.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Reflection;
+
+using NHibernate.Type;
+using NHibernate.UserTypes.SqlTypes;
+
+using NUnit.Framework;
+
+namespace NHibernate.UserTypes.Tests
+{
+	[TestFixture]
+	public class SerializabilityFixture
+	{
+		public static void CheckITypesInAssembly(Assembly assembly)
+		{
+			foreach (System.Type type in assembly.GetTypes())
+			{
+				if (type.IsClass && !type.IsAbstract && typeof(IType).IsAssignableFrom(type))
+				{
+					Assert.IsTrue(type.IsSerializable, "Type {0} should be serializable", type);
+				}
+			}
+		}
+
+		[Test]
+		public void SqlTypes()
+		{
+			CheckITypesInAssembly(typeof(SqlInt32Type).Assembly);
+		}
+	}
+}
