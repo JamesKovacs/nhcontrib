433e5e3596be8e5cb35e54fc74eff94f04ed14b0
karlchu <karlchu@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-02-25 00:14:38 +0000

    NHibernate.JetDriver - Moved here from NHibernate core
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@49 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.JetDriver/src/NHibernate.JetDriver/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Reflection;
+using System.Security;
+
+//using System.Runtime.CompilerServices;
+
+[assembly: CLSCompliantAttribute(true)]
+[assembly: AssemblyTitleAttribute("NHibernate.JetDriver")]
+[assembly: AssemblyDescriptionAttribute("The Jet driver and dialect for NHibernate 2.0.0.Alpha1.")]
+[assembly: AssemblyCompanyAttribute("NHibernate.org")]
+[assembly: AssemblyProductAttribute("NHibernate.JetDriver")]
+[assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
+[assembly: AssemblyVersionAttribute("2.0.0.1001")]
+[assembly: AssemblyInformationalVersionAttribute("2.0.0.1001")]
+[assembly: AssemblyFileVersionAttribute("2.0.0.1001")]
+//[assembly: AssemblyKeyFileAttribute("../../src/NHibernate.snk")]
+[assembly: AssemblyDelaySignAttribute(false)]
+[assembly: AllowPartiallyTrustedCallers()]
