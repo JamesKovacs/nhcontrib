f76288d9a4ea6ae2ea76a78bc47c17d9368a5088
karlchu <karlchu@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-02-26 04:52:21 +0000

    NHibernate.Tool.hbm2net - Moved here from NHibernate core
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@54 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.Tool.hbm2net/src/NHibernate.Tool.hbm2net.Tests/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Reflection;
+
+[assembly: CLSCompliantAttribute(true)]
+[assembly: AssemblyTitleAttribute("NHibernate.Tool.hbm2net.Tests")]
+[assembly: AssemblyDescriptionAttribute("Tests for NHibernate.Tool.hbm2net tool")]
+[assembly: AssemblyCompanyAttribute("NHibernate.org")]
+[assembly: AssemblyProductAttribute("NHibernate.Tool.hbm2net.Tests")]
+[assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
+[assembly: AssemblyVersionAttribute("0.1.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.1")]
+[assembly: AssemblyFileVersionAttribute("2.0.0.1001")]
+//[assembly: AssemblyKeyFileAttribute("../../src/NHibernate.snk")]
+//[assembly: AssemblyDelaySignAttribute(false)]
+
---
+++ src/NHibernate.Tool.hbm2net/src/NHibernate.Tool.hbm2net/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Reflection;
+
+[assembly: CLSCompliantAttribute(false)]
+[assembly: AssemblyTitleAttribute("NHibernate.Tool.hbm2net")]
+[assembly: AssemblyDescriptionAttribute("Library to Generate Classes from NHibernate 2.0.0.Alpha1 hbm.xml files.")]
+[assembly: AssemblyCompanyAttribute("NHibernate.org")]
+[assembly: AssemblyProductAttribute("NHibernate.Tool.hbm2net")]
+[assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
+[assembly: AssemblyVersionAttribute("2.0.0.1001")]
+[assembly: AssemblyInformationalVersionAttribute("2.0.0.1001")]
+[assembly: AssemblyFileVersionAttribute("2.0.0.1001")]
+//[assembly: AssemblyKeyFileAttribute("../../src/NHibernate.snk")]
+//[assembly: AssemblyDelaySignAttribute(false)]
+
