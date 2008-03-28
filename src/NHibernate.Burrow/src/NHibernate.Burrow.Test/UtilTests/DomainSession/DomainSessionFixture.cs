8b6e5d9eb3420da0543d52e05aca48fdab00e4d7
kailuowang <kailuowang@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-03-28 03:36:07 +0000

    Removed incorrect port of uNHAddins
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@100 d7b3437e-3345-0410-94a8-cbd290e69f67

--- src/NHibernate.Burrow/src/NHibernate.Burrow.Test/UtilTests/DomainSession/DomainSessionFixture.cs
+++ src/NHibernate.Burrow/src/NHibernate.Burrow.Test/UtilTests/DomainSession/DomainSessionFixture.cs
@@ -1,7 +1,8 @@
 using NHibernate.Burrow.TestUtil;
 using NUnit.Framework;
 
-namespace NHibernate.Burrow.Test.UtilTests {
+namespace NHibernate.Burrow.Test.UtilTests.DomainSession
+{
     [TestFixture]
     public class DomainSessionFixture : TestBase {
         [Test, Ignore("DomainSession feature was temporarily disabled")]
