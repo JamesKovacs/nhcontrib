3d8da93dd3a82b9c5c38786067a198c8f3e47ec3
diegojancic <diegojancic@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-08-02 07:37:48 +0000

    Fixed NHCH-10: Added indeXus.Net Shared Cache with small modifications to make it work for the NH trunk (Implementation provided by dabuddhaman)
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@475 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.Caches.SharedCache/src/NHibernate.Caches.SharedCache.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NHibernate.Caches.SharedCache.Tests-2.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Heilig-Hartziekenhuis vzw")]
+[assembly: AssemblyProduct("NHibernate.Caches.SharedCache.Tests-2.0")]
+[assembly: AssemblyCopyright("Copyright © Heilig-Hartziekenhuis vzw 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f36e9efc-172f-4d38-84fd-320f3659635b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
