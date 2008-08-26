89cad5ef8e70d9fba8336b517f4fc64b8f71516f
darioquintana <darioquintana@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-08-26 02:22:53 +0000

    More Impl on Second pass commands.
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@521 d7b3437e-3345-0410-94a8-cbd290e69f67

--- sandbox/dario/NHibernate.Annotations/NHibernate.Annotations/NHibernate.Cfg/IndexOrUniqueKeySecondPass.cs
+++ sandbox/dario/NHibernate.Annotations/NHibernate.Annotations/NHibernate.Cfg/IndexOrUniqueKeySecondPass.cs
@@ -1,17 +1,70 @@
 ﻿using System.Collections.Generic;
+using NHibernate.Annotations;
+using NHibernate.Annotations.NHibernate.Cfg;
 using NHibernate.Mapping;
 
 namespace NHibernate.Cfg
 {
-    public class IndexOrUniqueKeySecondPass
-    {
-        public IndexOrUniqueKeySecondPass(string indexName, Ejb3Column ejb3Column, ExtendedMappings mappings, bool b)
-        {
-            
-        }
+	public class IndexOrUniqueKeySecondPass : ISecondPass
+	{
+		private Table table;
+		private readonly string indexName;
+		private readonly string[] columns;
+		private readonly ExtendedMappings mappings;
+		private readonly Ejb3Column column;
+		private readonly bool unique;
 
-        public void DoSecondPass(IDictionary<string, PersistentClass> persistentClasses)
-        {
-        }
-    }
+		public IndexOrUniqueKeySecondPass(Table table, string indexName, string[] columns, ExtendedMappings mappings)
+		{
+			this.table = table;
+			this.indexName = indexName;
+			this.columns = columns;
+			this.mappings = mappings;
+			column = null;
+			unique = false;
+		}
+
+		public IndexOrUniqueKeySecondPass(string indexName, Ejb3Column column, ExtendedMappings mappings)
+			: this(indexName, column, mappings, false)
+		{
+		}
+
+		public IndexOrUniqueKeySecondPass(string indexName, Ejb3Column column, ExtendedMappings mappings, bool unique)
+		{
+			this.indexName = indexName;
+			this.column = column;
+			columns = null;
+			this.mappings = mappings;
+			this.unique = unique;
+		}
+        
+		public void DoSecondPass(IDictionary<string, PersistentClass> persistentClasses)
+		{
+			if (columns != null)
+			{
+				foreach (var columnName in columns)
+				{
+					AddConstraintToColumn(columnName);
+				}
+			}
+			if (column != null)
+			{
+				table = column.Table;
+				AddConstraintToColumn(mappings.GetLogicalColumnName(column.Name, table));
+			}
+		}
+
+		private void AddConstraintToColumn(string columnName)
+		{
+			Column column = table.GetColumn(new Column(mappings.GetPhysicalColumnName(columnName, table)));
+			
+			if (column == null)
+				throw new AnnotationException("@Index references a unknown column: " + columnName);
+			
+			if (unique)
+				table.GetOrCreateUniqueKey(indexName).AddColumn(column);
+			else
+				table.GetOrCreateIndex(indexName).AddColumn(column);
+		}
+	}
 }
