10e7916c9dd1133ab12ef98415f28a15d64aa960
wcpierce <wcpierce@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-09-21 23:21:57 +0000

    Added additional examples.
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@559 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.ProxyGenerators/examples/src/Example.Test/PrematureOptimizationTester.cs
@@ -0,0 +1,91 @@
+namespace Example.Test
+{
+	using System;
+	using Domain.Models;
+	using NHibernate;
+	using NHibernate.Cache;
+	using NHibernate.Cfg;
+	using NHibernate.Dialect;
+	using NUnit.Framework;
+	using System.Diagnostics;
+
+	[TestFixture]
+	public class PrematureOptimizationTester
+	{
+		[Test]
+		public void Static_Proxies_Outperform_Standard_Proxies()
+		{
+			Stopwatch sw = new Stopwatch();
+			const int instances = 100;
+
+			long time;
+			long timeWithStaticProxies;
+
+			sw.Reset();
+			using(ISessionFactory factory = CreateSessionFactory())
+			{
+				using(ISession session = factory.OpenSession())
+				{
+					sw.Start();
+
+					for (int i = 0; i < instances; i++)
+					{
+						session.Load<Person>(i);
+					}
+
+					sw.Stop();
+					time = sw.ElapsedMilliseconds;
+				}
+			}
+
+			sw.Reset();
+			using (ISessionFactory factory = CreateSessionFactoryWithStaticProxies())
+			{
+				using (ISession session = factory.OpenSession())
+				{
+					sw.Start();
+
+					for (int i = 0; i < instances; i++)
+					{
+						session.Load<Person>(i);
+					}
+
+					sw.Stop();
+					timeWithStaticProxies = sw.ElapsedMilliseconds;
+				}
+			}
+
+			Console.WriteLine("Time for {0} Instances: {1}ms", instances, time);
+			Console.WriteLine("Time (static proxies) for {0} Instances: {1}ms", instances, timeWithStaticProxies);
+			Assert.Less(timeWithStaticProxies, time);
+		}
+
+		private static ISessionFactory CreateSessionFactory()
+		{
+			return CreateSessionFactory(false);
+		}
+
+		private static ISessionFactory CreateSessionFactoryWithStaticProxies()
+		{
+			return CreateSessionFactory(true);
+		}
+
+		private static ISessionFactory CreateSessionFactory(bool useStaticProxies)
+		{
+			Configuration cfg = new Configuration();
+			cfg.Properties["cache.provider_class"] = typeof(HashtableCacheProvider).AssemblyQualifiedName;
+			cfg.Properties["dialect"] = typeof(MsSql2000Dialect).AssemblyQualifiedName;
+			
+			if (useStaticProxies)
+			{
+				cfg.Properties["proxyfactory.factory_class"] = typeof(CastleStaticProxyFactoryFactory).AssemblyQualifiedName;
+			}
+
+			cfg.AddAssembly(typeof(Person).Assembly);
+
+			return cfg.BuildSessionFactory();
+		}
+
+		
+	}
+}
