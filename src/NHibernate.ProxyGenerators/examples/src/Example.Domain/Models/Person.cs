a09506778041b3489ff426f46dc6f7726c78ba2d
wcpierce <wcpierce@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-09-26 17:22:34 +0000

    Reverted back to rev 552
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@592 d7b3437e-3345-0410-94a8-cbd290e69f67

--- src/NHibernate.ProxyGenerators/examples/src/Example.Domain/Models/Person.cs
+++ src/NHibernate.ProxyGenerators/examples/src/Example.Domain/Models/Person.cs
@@ -1,30 +1,25 @@
-﻿namespace Example.ActiveRecordDomain.Models
+﻿namespace Example.Domain.Models
 {
 	using System.Collections.Generic;
-	using Castle.ActiveRecord;
 
-	[ActiveRecord]
-	public class Person : ActiveRecordBase<Person>
+	public class Person
 	{
 		private int _id;
 		private string _name;
 		private IList<PhoneNumber> _phoneNumbers = new List<PhoneNumber>();
 
-		[PrimaryKey]
 		public virtual int Id
 		{
 			get { return _id; }
 			set { _id = value; }
 		}
 
-		[Property]
 		public virtual string Name
 		{
 			get { return _name; }
 			set { _name = value; }
 		}
 
-		[HasMany]
 		public virtual IList<PhoneNumber> PhoneNumbers
 		{
 			get { return _phoneNumbers; }
--- src/NHibernate.ProxyGenerators/examples/src/Example.Domain/Models/PhoneNumber.cs
+++ src/NHibernate.ProxyGenerators/examples/src/Example.Domain/Models/PhoneNumber.cs
@@ -1,37 +1,30 @@
-﻿namespace Example.ActiveRecordDomain.Models
+﻿namespace Example.Domain.Models
 {
-	using Castle.ActiveRecord;
-
-	[ActiveRecord]
-	public class PhoneNumber : ActiveRecordBase<PhoneNumber>
+	public class PhoneNumber
 	{
 		private int _id;
 		private Person _person;
 		private string _number;
 		private string _phoneType;
 
-		[PrimaryKey]
 		public virtual int Id
 		{
 			get { return _id; }
 			set { _id = value; }
 		}
 
-		[BelongsTo]
 		public virtual Person Person
 		{
 			get { return _person; }
 			set { _person = value; }
 		}
 
-		[Property]
 		public virtual string Number
 		{
 			get { return _number; }
 			set { _number = value; }
 		}
 
-		[Property]
 		public virtual string PhoneType
 		{
 			get { return _phoneType; }
