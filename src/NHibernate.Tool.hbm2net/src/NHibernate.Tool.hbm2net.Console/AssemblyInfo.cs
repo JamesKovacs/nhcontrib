5d32599ae6f1a6c256e5f9217f92daaac0266081
felpo <felpo@d7b3437e-3345-0410-94a8-cbd290e69f67>
2009-11-30 15:47:15 +0000

    updated to NH 2.1.2 GA
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1215 d7b3437e-3345-0410-94a8-cbd290e69f67

--- src/NHibernate.Tool.hbm2net/src/NHibernate.Tool.hbm2net.Console/AssemblyInfo.cs
+++ src/NHibernate.Tool.hbm2net/src/NHibernate.Tool.hbm2net.Console/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Reflection;
 [assembly: AssemblyProductAttribute("NHibernate.Tool.hbm2net.Console")]
 [assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
 [assembly: AssemblyVersionAttribute("2.1.0.4000")]
-[assembly: AssemblyInformationalVersionAttribute("2.1.0.4000")]
-[assembly: AssemblyFileVersionAttribute("2.1.0.4000")]
+[assembly: AssemblyInformationalVersionAttribute("2.1.2.4000")]
+[assembly: AssemblyFileVersionAttribute("2.1.2.4000")]
 //[assembly: AssemblyKeyFileAttribute("../../src/NHibernate.snk")]
 //[assembly: AssemblyDelaySignAttribute(false)]
 
