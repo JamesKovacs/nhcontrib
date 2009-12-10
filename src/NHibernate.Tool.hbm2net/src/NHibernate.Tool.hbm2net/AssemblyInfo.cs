a5e352b2db339e60f0cf403620761646863dc922
felpo <felpo@d7b3437e-3345-0410-94a8-cbd290e69f67>
2009-12-10 11:40:15 +0000

    hbm2net:little refactoring
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1224 d7b3437e-3345-0410-94a8-cbd290e69f67

--- src/NHibernate.Tool.hbm2net/src/NHibernate.Tool.hbm2net/AssemblyInfo.cs
+++ src/NHibernate.Tool.hbm2net/src/NHibernate.Tool.hbm2net/AssemblyInfo.cs
@@ -3,13 +3,13 @@ using System.Reflection;
 
 [assembly: CLSCompliantAttribute(false)]
 [assembly: AssemblyTitleAttribute("NHibernate.Tool.hbm2net")]
-[assembly: AssemblyDescriptionAttribute("Library to Generate Classes from NHibernate 2.0.0.Alpha1 hbm.xml files.")]
+[assembly: AssemblyDescriptionAttribute("Library to Generate Classes from NHibernate 2.x.x hbm.xml files.")]
 [assembly: AssemblyCompanyAttribute("NHibernate.org")]
 [assembly: AssemblyProductAttribute("NHibernate.Tool.hbm2net")]
 [assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
-[assembly: AssemblyVersionAttribute("2.1.2.4000")]
-[assembly: AssemblyInformationalVersionAttribute("2.1.2.4000")]
-[assembly: AssemblyFileVersionAttribute("2.1.2.4000")]
+[assembly: AssemblyVersionAttribute("1.0.2.1000")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.2.1000")]
+[assembly: AssemblyFileVersionAttribute("1.0.2.1000")]
 //[assembly: AssemblyKeyFileAttribute("../../src/NHibernate.snk")]
 //[assembly: AssemblyDelaySignAttribute(false)]
 
