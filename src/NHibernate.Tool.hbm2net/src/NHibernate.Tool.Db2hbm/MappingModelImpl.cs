15e51fa85a8e085c36a1d8bd27af93bbd44c5679
felpo <felpo@d7b3437e-3345-0410-94a8-cbd290e69f67>
2010-01-04 20:52:27 +0000

    db2hbm: fixed missing file 'mappingmodelimpl.cs'
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1235 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.Tool.hbm2net/src/NHibernate.Tool.Db2hbm/MappingModelImpl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernate.Tool.Db2hbm
+{
+    class MappingModelImpl:IMappingModel
+    {
+        Dictionary<string, @class> entityMap = new Dictionary<string, @class>();
+        #region IMappingModel Members
+
+        public @class AddClassForTable(string tableName, string entityName)
+        {
+            @class clazz = new @class();
+            clazz.name = entityName;
+            clazz.table = tableName;
+            entityMap[tableName] = clazz;
+            return clazz;
+        }
+
+        
+        public IList<@class> GetEntities()
+        {
+            return entityMap.Values.ToArray();
+        }
+
+        #endregion
+    }
+}
