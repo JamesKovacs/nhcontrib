55792b510dfa1282d90cfbc4a7c2de52282acf89
felpo <felpo@d7b3437e-3345-0410-94a8-cbd290e69f67>
2010-01-05 16:52:13 +0000

    db2hbm: some progress in infrastructure.
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1237 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.Tool.hbm2net/src/NHibernate.Tool.Db2hbm/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Common;
+
+namespace NHibernate.Tool.Db2hbm
+{
+    static class Extensions
+    {
+        public static bool ParseFromDb(this bool boolean,string data)
+        {
+            data = data.ToLower();
+            return data == "yes" || data == "true" || data == "1";
+        }
+
+        public static DataTable GetPrimaryKey(this NHibernate.Dialect.Dialect dialect, DbConnection connection, string schema, string table)
+        {
+            DbCommand command = connection.CreateCommand();
+            command.CommandText = "select * from " + GetTableName(schema,table,dialect);
+            using (DbDataReader reader = command.ExecuteReader(CommandBehavior.SchemaOnly | CommandBehavior.KeyInfo))
+            {
+                return reader.GetSchemaTable();
+            }
+        }
+        private static string GetTableName(string schema, string table, NHibernate.Dialect.Dialect dialect)
+        {
+
+            if (!string.IsNullOrEmpty(schema))
+            {
+                return string.Format("{0}.{1}", dialect.QuoteForTableName(schema), dialect.QuoteForTableName(table));
+            }
+            else
+            {
+                return dialect.QuoteForTableName(table);
+            }
+        }
+    }
+}
