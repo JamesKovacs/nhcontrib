b16e7cb92b290e432394e74eddc62b679696794e
felpo <felpo@d7b3437e-3345-0410-94a8-cbd290e69f67>
2010-03-24 11:44:06 +0000

    cleanup
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1275 d7b3437e-3345-0410-94a8-cbd290e69f67

--- src/NHibernate.Tool.hbm2net/src/NHibernate.Tool.Db2hbm/BooDecoratorAttribute.cs
+++ src/NHibernate.Tool.hbm2net/src/NHibernate.Tool.Db2hbm/BooDecoratorAttribute.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NHibernate.Hql.Ast.ANTLR;
 
 namespace NHibernate.Tool.Db2hbm
 {
     [AttributeUsage(AttributeTargets.Property,AllowMultiple=false)]
     public class BooDecoratorAttribute:Attribute
     {
-
+        public BooDecoratorAttribute()
+        {
+           
+            
+        }
     }
 }
