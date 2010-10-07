2ab07ccfd2cd207bb89332d0898136223ba05e5a
rogerkratz <rogerkratz@d7b3437e-3345-0410-94a8-cbd290e69f67>
2010-10-07 15:02:36 +0000

    added NonVersionedTest
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1359 d7b3437e-3345-0410-94a8-cbd290e69f67

--- sandbox/simondud/Envers.NET/Envers.NET.Tests/Integration/Basic/Model.cs
+++ sandbox/simondud/Envers.NET/Envers.NET.Tests/Integration/Basic/Model.cs
@@ -23,6 +23,26 @@ namespace Envers.NET.Tests.Integration.Basic
         }
     }
 
+    public class BasicTestEntity3
+    {
+        public virtual int Id { get; set; }
+        public virtual string Str1 { get; set; }
+        public virtual string Str2 { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var bte = obj as BasicTestEntity3;
+            if (bte == null)
+                return false;
+            return (bte.Id == Id && string.Equals(bte.Str1, Str1) && string.Equals(bte.Str2, Str2));
+        }
+
+        public override int GetHashCode()
+        {
+            return Id ^ Str1.GetHashCode() ^ Str2.GetHashCode();
+        }
+    }
+
     [Audited]
     public class BasicTestEntity4
     {
