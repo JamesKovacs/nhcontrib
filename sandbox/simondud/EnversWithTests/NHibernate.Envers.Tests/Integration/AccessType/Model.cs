c2b1d3a3d1fe7d0e7abf16020f2a99beaf6cd1ad
rogerkratz <rogerkratz@d7b3437e-3345-0410-94a8-cbd290e69f67>
2010-10-13 14:59:12 +0000

    adding MixedAccessTest
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1392 d7b3437e-3345-0410-94a8-cbd290e69f67

--- sandbox/simondud/EnversWithTests/NHibernate.Envers.Tests/Integration/AccessType/Model.cs
+++ sandbox/simondud/EnversWithTests/NHibernate.Envers.Tests/Integration/AccessType/Model.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace NHibernate.Envers.Tests.Integration.AccessType
 {
     public class FieldAccessEntity
@@ -37,5 +39,96 @@ namespace NHibernate.Envers.Tests.Integration.AccessType
         public virtual int Id { get; set; }
         [Audited]
         public virtual string Data { get; set; }
+    }
+
+    [Audited]
+    public class Country
+    {
+        private readonly int _code;
+        private readonly string _name;
+
+        protected Country()
+        {
+        }
+
+        public Country(int code, string name)
+        {
+            _code = code;
+            _name = name;
+        }
+
+        public virtual int Code
+        {
+            get { return _code; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var casted = obj as Country;
+            if (casted == null)
+                return false;
+
+            return casted.Code == Code && casted._name.Equals(_name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Code ^ _name.GetHashCode();
+        }
+    }
+
+    public class MixedAccessEntity
+    {
+        private int id;
+        private string data;
+
+        protected MixedAccessEntity(){}
+
+        public MixedAccessEntity(string data)
+        {
+            this.data = data;
+        }
+
+        public MixedAccessEntity(int id, string data)
+        {
+            this.id = id;
+            this.data = data;
+        }
+
+        [Audited]
+        protected virtual string Data
+        {
+            get
+            {
+                return data;
+            } 
+            set
+            {
+                IsDataSet = true;
+                data = value;
+            }
+        }
+
+        public virtual bool IsDataSet { get; private set; }
+
+        public virtual void WriteData(string data)
+        {
+            IsDataSet = true;
+            this.data = data;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var casted = obj as MixedAccessEntity;
+            if (casted == null)
+                return false;
+
+            return casted.id == id && casted.data == data;
+        }
+
+        public override int GetHashCode()
+        {
+            return id ^ data.GetHashCode();
+        }
     }
 }
