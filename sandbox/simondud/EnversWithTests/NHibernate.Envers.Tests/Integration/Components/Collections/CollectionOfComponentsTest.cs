da7f72315914b5bc94a423fa6c4fd10891913e78
rogerkratz <rogerkratz@d7b3437e-3345-0410-94a8-cbd290e69f67>
2010-11-02 16:20:37 +0000

    adding CollectionOfComponentsTest. It fails now, will check this soon.
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1426 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ sandbox/simondud/EnversWithTests/NHibernate.Envers.Tests/Integration/Components/Collections/CollectionOfComponentsTest.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using NHibernate.Envers.Tests.Entities.Components;
+using NUnit.Framework;
+
+namespace NHibernate.Envers.Tests.Integration.Components.Collections
+{
+	[TestFixture]
+	public class CollectionOfComponentsTest : TestBase
+	{
+		private int id1;
+
+		protected override IEnumerable<string> Mappings
+		{
+			get { return new[] { "Entities.Components.Mapping.hbm.xml" }; }
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			var cte1 = new ComponentSetTestEntity();
+			using(var tx = Session.BeginTransaction())
+			{
+				id1 = (int)Session.Save(cte1);
+				tx.Commit();
+			}
+			using(var tx = Session.BeginTransaction())
+			{
+				cte1.Comps.Add(new Component1 {Str1 = "a", Str2 = "b"});
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void VerifyRevisionCounts()
+		{
+			CollectionAssert.AreEqual(new[] { 1, 2 }, AuditReader.GetRevisions(typeof(ComponentSetTestEntity), id1));
+		}
+
+		[Test]
+		public void VerifyHistoryOf1()
+		{
+			CollectionAssert.IsEmpty(AuditReader.Find<ComponentSetTestEntity>(id1, 1).Comps);
+
+			var comps1 = AuditReader.Find<ComponentSetTestEntity>(id1, 2).Comps;
+			Assert.AreEqual(1, comps1.Count);
+			CollectionAssert.Contains(comps1, new Component1 { Str1 = "a", Str2 = "b" });
+		}
+	}
+}
