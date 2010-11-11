8259d9634090e7d886a6ad054e65970946079dd7
felpo <felpo@d7b3437e-3345-0410-94a8-cbd290e69f67>
2010-11-11 12:05:37 +0000

    Added the vs2010 addin project
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1433 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.Tool.hbm2net/src/Hbm2NetAddIn/Hbm2NetAddIn/Helpers/RdtManager.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.Shell.Interop;
+using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using System.IO;
+
+namespace FelicePollano.Hbm2NetAddIn.Helpers
+{
+    class RdtManager:IVsRunningDocTableEvents
+    {
+        uint cookie;
+        IVsRunningDocumentTable rdt;
+        public event EventHandler<SavedProjectItemEventArgs> SavedProjectItem;
+        public RdtManager(IServiceProvider provider)
+        {
+
+            rdt = provider.GetService(typeof(IVsRunningDocumentTable)) as IVsRunningDocumentTable;
+            rdt.AdviseRunningDocTableEvents(this, out cookie);
+            var dte = provider.GetService(typeof(DTE)) as DTE;
+            
+        }
+
+        #region IVsRunningDocTableEvents Members
+
+        public int OnAfterAttributeChange(uint docCookie, uint grfAttribs)
+        {
+            return 0;
+        }
+
+        public int OnAfterDocumentWindowHide(uint docCookie, IVsWindowFrame pFrame)
+        {
+            return 0;
+        }
+
+        public int OnAfterFirstDocumentLock(uint docCookie, uint dwRDTLockType, uint dwReadLocksRemaining, uint dwEditLocksRemaining)
+        {
+            return 0;
+        }
+
+        public int OnAfterSave(uint docCookie)
+        {
+            string path;
+            IVsHierarchy hierarchy;
+            uint flags, pdwReadLocks, pdwEditLocks, pitemid;
+            IntPtr ppunkDocData;
+            rdt.GetDocumentInfo(docCookie, out flags, out pdwReadLocks, out pdwEditLocks, out path, out hierarchy, out pitemid, out ppunkDocData);
+            var prj = GetProjectItem(hierarchy,pitemid);
+          
+            if (null != SavedProjectItem)
+            {
+                SavedProjectItem(this, new SavedProjectItemEventArgs() {  ProjectItem=prj, SavedPath=path });
+            }
+            return 0;
+        }
+        public ProjectItem GetProjectItem(IVsHierarchy hierarchy,uint itemid)
+        {
+
+            object project;
+
+
+
+            ErrorHandler.ThrowOnFailure
+
+                (hierarchy.GetProperty(
+                    itemid,
+                    (int)__VSHPROPID.VSHPROPID_ExtObject,
+                    out project));
+
+            return (project as ProjectItem);
+
+        }
+        public Project GetProject(IVsHierarchy hierarchy)
+        {
+
+            object project;
+
+            ErrorHandler.ThrowOnFailure
+                (hierarchy.GetProperty(
+                    (uint)VSConstants.VSITEMID.Root,
+                    (int)__VSHPROPID.VSHPROPID_ExtObject,
+                    out project));
+            return (project as Project);
+
+        }
+
+        public int OnBeforeDocumentWindowShow(uint docCookie, int fFirstShow, IVsWindowFrame pFrame)
+        {
+            return 0;
+        }
+
+        public int OnBeforeLastDocumentUnlock(uint docCookie, uint dwRDTLockType, uint dwReadLocksRemaining, uint dwEditLocksRemaining)
+        {
+            return 0;
+        }
+
+        #endregion
+    }
+}
---
+++ src/NHibernate.Tool.hbm2net/src/Hbm2NetAddIn/Hbm2NetAddIn/Helpers/SavedProjectItemEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnvDTE;
+
+namespace FelicePollano.Hbm2NetAddIn.Helpers
+{
+    public class SavedProjectItemEventArgs:EventArgs
+    {
+        public ProjectItem ProjectItem { get; set; }
+        public string SavedPath { get; set; }
+    }
+}
