ffd2e94b35790723ea2947d79e54fbc0cb3ecf76
felpo <felpo@d7b3437e-3345-0410-94a8-cbd290e69f67>
2010-11-11 13:16:43 +0000

    Added the vs2010 addin project
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1435 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.Tool.hbm2net/src/Hbm2NetAddIn/Hbm2NetRunner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FelicePollano.Hbm2NetAddIn
+{
+    public class Hbm2NetRunner
+    {
+    }
+}
