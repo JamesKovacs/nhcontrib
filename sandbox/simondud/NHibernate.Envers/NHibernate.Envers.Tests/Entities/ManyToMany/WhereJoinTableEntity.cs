15d50f908a3ee5e5478c37f42163fd879462c79e
rogerkratz <rogerkratz@d7b3437e-3345-0410-94a8-cbd290e69f67>
2011-01-04 12:01:29 +0000

    adding BasicWhereJoinTableTest
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1549 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ sandbox/simondud/NHibernate.Envers/NHibernate.Envers.Tests/Entities/ManyToMany/WhereJoinTableEntity.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace NHibernate.Envers.Tests.Entities.ManyToMany
+{
+	[Audited]
+	public class WhereJoinTableEntity
+	{
+		public virtual int Id { get; set; }
+		public virtual string Data { get; set; }
+		public virtual IList<IntNoAutoIdTestEntity> References1 { get; set; }
+		public virtual IList<IntNoAutoIdTestEntity> References2 { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var casted = obj as WhereJoinTableEntity;
+			if (casted == null)
+				return false;
+			return (Id == casted.Id && Data == casted.Data);
+		}
+
+		public override int GetHashCode()
+		{
+			return Id ^ Data.GetHashCode();
+		}
+	}
+}
