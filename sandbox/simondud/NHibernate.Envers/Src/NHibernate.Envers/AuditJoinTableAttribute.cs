99843b081c2fafb40bb3715be088e4b27844f849
rogerkratz <rogerkratz@d7b3437e-3345-0410-94a8-cbd290e69f67>
2011-01-05 13:29:33 +0000

    copy source file to src folder
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1555 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ sandbox/simondud/NHibernate.Envers/Src/NHibernate.Envers/AuditJoinTableAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernate.Envers
+{
+    [AttributeUsage(AttributeTargets.Property|AttributeTargets.Method)]
+    public class AuditJoinTableAttribute : Attribute
+    {
+    /**
+     * @return Name of the join table. Defaults to a concatenation of the names of the primary table of the entity
+     * owning the association and of the primary table of the entity referenced by the association.
+     */
+    public String Name = "";
+
+    /**
+     * @return The schema of the join table. Defaults to the schema of the entity owning the association.
+     */
+    public String Schema = "";
+
+    /**
+     * @return The catalog of the join table. Defaults to the catalog of the entity owning the association.
+     */
+    public String Catalog = "";
+
+    /**
+     * @return The foreign key columns of the join table which reference the primary table of the entity that does not
+     * own the association (i.e. the inverse side of the association).
+     */
+    public JoinColumnAttribute[] InverseJoinColumns = {};
+    }
+}
