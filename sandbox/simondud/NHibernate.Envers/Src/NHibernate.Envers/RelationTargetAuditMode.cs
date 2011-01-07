c8610040389c447227dced610d1721a93387c897
rogerkratz <rogerkratz@d7b3437e-3345-0410-94a8-cbd290e69f67>
2011-01-07 10:33:32 +0000

    finished porting many2many tests
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk@1562 d7b3437e-3345-0410-94a8-cbd290e69f67

--- sandbox/simondud/NHibernate.Envers/Src/NHibernate.Envers/RelationTargetAuditMode.cs
+++ sandbox/simondud/NHibernate.Envers/Src/NHibernate.Envers/RelationTargetAuditMode.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NHibernate.Envers
+﻿namespace NHibernate.Envers
 {
     /**
      * @author Simon Duduica, port of Envers omonyme class by Adam Warski (adam at warski dot org)
